7f26f5e73c75b46afc4bd0827e1b7f82a2cd26fd
Quantum Snowball <quantum.snowball@gmail.com>
2022-09-16 21:50:27 +0800

    feat: mui styled component

--- components/App.tsx
+++ components/App.tsx
@@ -1,6 +1,22 @@
+import { styled } from '@mui/material'
+import { CenterContent } from './styled/containers'
+
+
+const FlexColumnDiv = styled(CenterContent('div'))`
+  /* cover full viewport */
+  height: 100vh;
+  max-height: 100vh;
+  justify-content: space-between;
+  /* theme */
+  color: ${props => props.theme.palette.text.primary};
+  background-color: ${props => props.theme.palette.background.default};
+`
+
 const App = () => {
   return (
-    <p> App Component </p>
+    <FlexColumnDiv className="app-ctn">
+      <p> App Component </p>
+    </FlexColumnDiv>
   )
 }
 
