8653543c71d275e11e8c3a7cc1930b1295455423
Quantum Snowball <quantum.snowball@gmail.com>
2022-09-18 03:26:44 +0800

    feat: add and delete todos

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,7 +1,7 @@
 import { createRouter } from 'next-connect'
 import { Request, Response } from '../../types/backend'
 import { defaultHandlerOptions } from '../../utils/backend'
-import { checkAccessToken, fetchTodos } from '../../utils/backend/resources'
+import { checkAccessToken, deleteTodo, fetchTodos, insertTodo } from '../../utils/backend/resources'
 
 
 const router = createRouter<Request, Response>()
@@ -10,6 +10,14 @@ router
   .post(
     checkAccessToken,
     fetchTodos
+  )
+  .put(
+    checkAccessToken,
+    insertTodo
+  )
+  .delete(
+    checkAccessToken,
+    deleteTodo
   )
 
 export default router.handler(defaultHandlerOptions)
--- utils/backend/resources.ts
+++ utils/backend/resources.ts
@@ -1,7 +1,7 @@
 import jwt, { TokenExpiredError } from 'jsonwebtoken'
 import db from './database/mongo'
 import { _Id } from '../../types'
-import { Middleware } from '../../types/backend'
+import { Middleware, TodoDocument } from '../../types/backend'
 import {
   ACCESS_TOKEN_SECRET,
   DATABASE,
@@ -42,33 +42,39 @@ export const fetchTodos: Middleware = async (req, res) => {
   return res.status(200).json({ payload: todos })
 }
 
-// export const insertTodo: RequestHandler = async (req, res) => {
-//   // decode to get back user infos
-//   const decoded = jwt.decode(req.cookies.accessToken)
-//   if (typeof decoded === 'string' || !decoded.hasOwnProperty('user'))
-//     return res.status(400).json({ message: 'Failed to decode refreshToken.' })
-//   // add a new entry to database
-//   const user = decoded.user
-//   const todo = req.body
-//   const doc: TodoDocument = { user, ...todo }
-//   await db.insertTodo(DATABASE, TODOS_COLLECTION, doc)
-//   // query database for the updated list
-//   const filter = { user }
-//   const todos = await db.findTodos(DATABASE, TODOS_COLLECTION, filter)
-//   return res.status(200).json({ payload: todos })
-// }
-//
-// export const deleteTodo: RequestHandler = async (req, res) => {
-//   // decode to get back user infos
-//   const decoded = jwt.decode(req.cookies.accessToken)
-//   if (typeof decoded === 'string' || !decoded.hasOwnProperty('user'))
-//     return res.status(400).json({ message: 'Failed to decode refreshToken.' })
-//   // remove the entry from collection
-//   const user = decoded.user
-//   const _id: _Id = req.body
-//   await db.deleteTodo(DATABASE, TODOS_COLLECTION, _id)
-//   // query database for the updated list
-//   const filter = { user }
-//   const todos = await db.findTodos(DATABASE, TODOS_COLLECTION, filter)
-//   return res.status(200).json({ payload: todos })
-// }
+export const insertTodo: Middleware = async (req, res) => {
+  const { accessToken } = req.cookies
+  if (!accessToken)
+    return res.status(401)
+  // decode to get back user infos
+  const decoded = jwt.decode(accessToken)
+  if (!decoded || typeof decoded === 'string' || !decoded.hasOwnProperty('user'))
+    return res.status(400).json({ message: 'Failed to decode refreshToken.' })
+  // add a new entry to database
+  const user = decoded.user
+  const todo = req.body
+  const doc: TodoDocument = { user, ...todo }
+  await db.insertTodo(DATABASE, TODOS_COLLECTION, doc)
+  // query database for the updated list
+  const filter = { user }
+  const todos = await db.findTodos(DATABASE, TODOS_COLLECTION, filter)
+  return res.status(200).json({ payload: todos })
+}
+
+export const deleteTodo: Middleware = async (req, res) => {
+  const { accessToken } = req.cookies
+  if (!accessToken)
+    return res.status(401)
+  // decode to get back user infos
+  const decoded = jwt.decode(accessToken)
+  if (!decoded || typeof decoded === 'string' || !decoded.hasOwnProperty('user'))
+    return res.status(400).json({ message: 'Failed to decode refreshToken.' })
+  // remove the entry from collection
+  const user = decoded.user
+  const _id: _Id = req.body
+  await db.deleteTodo(DATABASE, TODOS_COLLECTION, _id)
+  // query database for the updated list
+  const filter = { user }
+  const todos = await db.findTodos(DATABASE, TODOS_COLLECTION, filter)
+  return res.status(200).json({ payload: todos })
+}
