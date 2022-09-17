4a926e385859ee59dddfa2ffc7e8c238bf3f7975
Quantum Snowball <quantum.snowball@gmail.com>
2022-09-18 02:40:15 +0800

    refactor: mongo export a default object containing functions

--- utils/backend/database/mongo.ts
+++ utils/backend/database/mongo.ts
@@ -32,81 +32,62 @@ const operation = <R, P>(func: MongoOperation<R, P>) =>
     }
   }
 
-// insert pending user
-const upsertPendingUser = operation(
-  async (collection: Collection, filter: PendingUser) => {
-    await collection.deleteMany({ email: filter.email })
-    await collection.insertOne(filter)
-  }
-)
-
-// find pending user
-const findPendingUser = operation(
-  async (collection: Collection, filter: User) => {
-    return await collection.findOne<PendingUser>(filter)
-  }
-)
-
-// delete pending users
-const deletePendingUsers = operation(
-  async (collection: Collection, filter: User) => {
-    return await collection.deleteMany(filter)
-  }
-)
-
-// insert user
-const insertUser = operation(
-  async (collection: Collection, filter: UserWithPassword) => {
-    await collection.insertOne(filter)
-  }
-)
-
-// upsert oauth user
-const upsertOAuthUser = operation(
-  async (collection: Collection, filter: User) => {
-    await collection.updateOne(filter, { $setOnInsert: filter }, { upsert: true })
-  }
-)
-
-// find user
-const findUser = operation(
-  async (collection: Collection, filter: User) => {
-    return await collection.findOne<UserWithPassword>(filter)
-  }
-)
-
-// insert todo
-const insertTodo = operation(
-  async (collection: Collection, doc: TodoDocument) => {
-    await collection.insertOne(doc)
-  }
-)
-
-// delete todo
-const deleteTodo = operation(
-  async (collection: Collection, filter: _Id) => {
-    await collection.deleteOne({ _id: new ObjectId(filter._id) })
-  }
-)
-
-// find todo
-const findTodos = operation(
-  async (collection: Collection, filter: UserFilter) => {
-    const cursor = collection.find<TodoWithId>(filter, { projection: { title: 1, content: 1 } })
-    const found = await cursor.toArray()
-    return found
-  }
-)
-
 export default {
-  upsertPendingUser,
-  findPendingUser,
-  deletePendingUsers,
-  insertUser,
-  upsertOAuthUser,
-  findUser,
-  insertTodo,
-  deleteTodo,
-  findTodos
+  // insert pending user
+  upsertPendingUser: operation(
+    async (collection: Collection, filter: PendingUser) => {
+      await collection.deleteMany({ email: filter.email })
+      await collection.insertOne(filter)
+    }
+  ),
+  // find pending user
+  findPendingUser: operation(
+    async (collection: Collection, filter: User) => {
+      return await collection.findOne<PendingUser>(filter)
+    }
+  ),
+  // delete pending users
+  deletePendingUsers: operation(
+    async (collection: Collection, filter: User) => {
+      return await collection.deleteMany(filter)
+    }
+  ),
+  // insert user
+  insertUser: operation(
+    async (collection: Collection, filter: UserWithPassword) => {
+      await collection.insertOne(filter)
+    }
+  ),
+  // upsert oauth user
+  upsertOAuthUser: operation(
+    async (collection: Collection, filter: User) => {
+      await collection.updateOne(filter, { $setOnInsert: filter }, { upsert: true })
+    }
+  ),
+  // find user
+  findUser: operation(
+    async (collection: Collection, filter: User) => {
+      return await collection.findOne<UserWithPassword>(filter)
+    }
+  ),
+  // insert todo
+  insertTodo: operation(
+    async (collection: Collection, doc: TodoDocument) => {
+      await collection.insertOne(doc)
+    }
+  ),
+  // delete todo
+  deleteTodo: operation(
+    async (collection: Collection, filter: _Id) => {
+      await collection.deleteOne({ _id: new ObjectId(filter._id) })
+    }
+  ),
+  // find todo
+  findTodos: operation(
+    async (collection: Collection, filter: UserFilter) => {
+      const cursor = collection.find<TodoWithId>(filter, { projection: { title: 1, content: 1 } })
+      const found = await cursor.toArray()
+      return found
+    }
+  )
 }
-
